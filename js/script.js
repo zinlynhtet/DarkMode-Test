401a0b939eaa46349ee60b909dea55bad2d88eb8
MacFlick <mackk5504@gmail.com>
2023-12-06 22:30:43 +0630

    Add linters

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-const checkbox = document.querySelector('#checkbox')
+const checkbox = document.querySelector('#checkbox');
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  checkbox.setAttribute('checked', true)
+  checkbox.setAttribute('checked', true);
 }
-
-checkbox.addEventListener('change', function (event) {
+// eslint-disable-next-line no-unused-vars
+checkbox.addEventListener('change', function handleCheckboxChange(event) {
   if (this.checked) {
-    document.body.classList.remove('is-light-mode')
-    document.body.classList.add('is-dark-mode')
+    document.body.classList.remove('is-light-mode');
+    document.body.classList.add('is-dark-mode');
   } else {
-    document.body.classList.remove('is-dark-mode')
-    document.body.classList.add('is-light-mode')
+    document.body.classList.remove('is-dark-mode');
+    document.body.classList.add('is-light-mode');
   }
-})
+});
